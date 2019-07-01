24988756d06e5dfe08fa47367c8067743c9ffe20
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-01 12:52:38 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -5,7 +5,7 @@ export defult class Keypad extends React.Component
 {
   function passFunction()
   {
-
+    console.log('Entering password...');
   }
   render(){
     return(<div><input type='password' onKeyUp={this.passFunction}></div>)
