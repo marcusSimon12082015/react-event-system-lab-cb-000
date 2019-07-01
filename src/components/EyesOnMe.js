650f055446e10b5ce673b95f1ebdd1c0cf526f5f
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-01 14:57:59 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+export default class EyesOnMe extends React.Component 
+{
+  function focusOnMe(event)
+  {
+    const eventType = event.type;
+    
+  }
+  
+  render()
+  {
+    return(<div><button on></button></div>);
+  }
+}
